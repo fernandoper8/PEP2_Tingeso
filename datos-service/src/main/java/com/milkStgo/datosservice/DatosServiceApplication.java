762eb217404762542f4cbeb7c5d12d1ceb85def2
Fernando <fernando.perez.c@usach.cl>
2023-06-14 15:04:35 -0400

    Eureka Server y los Eureka Client
    Se modifica el pom para lo de eureka

--- datos-service/src/main/java/com/milkStgo/datosservice/DatosServiceApplication.java
+++ datos-service/src/main/java/com/milkStgo/datosservice/DatosServiceApplication.java
@@ -2,8 +2,10 @@ package com.milkStgo.datosservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DatosServiceApplication {
 
 	public static void main(String[] args) {
--- planilla-service/src/main/java/com/milkStgo/planillaservice/PlanillaServiceApplication.java
+++ planilla-service/src/main/java/com/milkStgo/planillaservice/PlanillaServiceApplication.java
@@ -2,8 +2,10 @@ package com.milkStgo.planillaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PlanillaServiceApplication {
 
 	public static void main(String[] args) {
